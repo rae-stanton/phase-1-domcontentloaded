1f2f91777c47cd2571c690a41f4a8f5d4343ae85
Rae Stanton <raestanton93@gmail.com>
2023-08-02 12:59:25 -0700

    event listeners

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+  const paragraph = document.querySelector('p');
+  paragraph.textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const paragraph = document.querySelector('p');
+  paragraph.textContent = "This is really cool!";
+});
